8df7928b89a52bbb5d2e5bc46a0ee565e85e6798
Aapo Kokko <aapo.kokko8@gmail.com>
2026-04-28 21:15:26 +0300

    Support treesitter in both 0.11 and 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,49 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
   config = function()
-    -- https://github.com/nvim-treesitter/nvim-treesitter#modules
-    require'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (c, lua, vim, vimdoc adn query should always be installed)
-      ensure_installed = {
-        'bash',
-        'diff',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'html',
-        'css',
-        'scss',
-        'php',
-        'javascript',
-        'typescript',
-        'json',
-        'yaml',
-        'hcl',
-        'terraform',
-        'dockerfile',
-        'c',
-        'lua',
-        'vim',
-        'vimdoc',
-        'query',
-      },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
+    local parsers = {
+      'bash',
+      'diff',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'html',
+      'css',
+      'scss',
+      'php',
+      'javascript',
+      'typescript',
+      'json',
+      'yaml',
+      'hcl',
+      'terraform',
+      'dockerfile',
+      'c',
+      'lua',
+      'vim',
+      'vimdoc',
+      'query',
     }
+
+    -- New API (nvim-treesitter 1.x, Neovim 0.12+)
+    -- Old API (nvim-treesitter 0.x, Neovim 0.11)
+    local ok, configs = pcall(require, 'nvim-treesitter.configs')
+    if ok then
+      configs.setup {
+        ensure_installed = parsers,
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    else
+      require('nvim-treesitter').setup {
+        ensure_installed = parsers,
+        auto_install = true,
+      }
+    end
   end
 }
